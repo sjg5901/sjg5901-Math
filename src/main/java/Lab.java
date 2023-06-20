ba93adfb39777fff32646986d79fa874a8a62f45
Skyler Giannetti <sjg5901@psu.edu>
2023-06-20 04:37:56 +0000

    Math lab comp #2

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,7 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
+        
         return Math.pow(a,b);
     }
 }
